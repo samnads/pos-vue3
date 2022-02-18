4b7c8aac30e31de1a2ace78dbc5e45b7bc25ae1d
Samnad Sainulabdeen <samnads2014@gmail.com>
2022-02-18 19:56:24 +0530

    vs code

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdminLogin from '../admin/AdminLogin.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminLogin
   },
   {
     path: '/about',
