6ff22dcb9331f41aa18b0d3f6ec9cd16566fe83d
Samnad Sainulabdeen <samnads2014@gmail.com>
2022-02-21 11:02:27 +0530

    vs code

---
+++ src/admin/AdminTest.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">
+    
+      <!--<h2>Admin</h2>
+       <router-link to="/admin/profile">Profile</router-link> |
+       <router-link to="/admin/login">Login</router-link> |-->
+      <router-view></router-view>
+    </div>
+</template>
