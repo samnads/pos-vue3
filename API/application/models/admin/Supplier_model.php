3c62479ba302837ce5543596cf5547bf7b0fcb76
Samnad Sainulabdeen <samnads2014@gmail.com>
2023-01-03 22:48:30 +0530

    laravel

--- API/application/models/admin/Supplier_model.php
+++ API/application/models/admin/Supplier_model.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed'); 
 class Supplier_model extends CI_Model
 {
     function __construct()
