a3fa5bda6ba58439a07548d0f35bd02f91dad585
Samnad Sainulabdeen <samnads2014@gmail.com>
2023-01-10 23:20:22 +0530

    vs code

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@
 const config = (() => {
     return {
         "VUE_APP_ROOT": "http://example.com/",
-        "VUE_APP_SUB_PATH": "pos-vue3-build",
+        "VUE_APP_SUB_PATH": "",
         "VUE_APP_API_ROOT": "http://localhost/pos-vue3/API/",
     };
 })();
