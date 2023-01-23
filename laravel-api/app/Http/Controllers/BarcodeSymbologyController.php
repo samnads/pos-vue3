be600368c32a08cd3a7a3c09c52ba69ea6103999
Samnad Sainulabdeen <samnads2014@gmail.com>
2023-01-23 22:58:00 +0530

    vs code

---
+++ laravel-api/app/Http/Controllers/BarcodeSymbologyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BarcodeSymbology;
+use Illuminate\Http\Request;
+
+class BarcodeSymbologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BarcodeSymbology  $barcodeSymbology
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BarcodeSymbology $barcodeSymbology)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BarcodeSymbology  $barcodeSymbology
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BarcodeSymbology $barcodeSymbology)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BarcodeSymbology  $barcodeSymbology
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BarcodeSymbology $barcodeSymbology)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BarcodeSymbology  $barcodeSymbology
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BarcodeSymbology $barcodeSymbology)
+    {
+        //
+    }
+}
